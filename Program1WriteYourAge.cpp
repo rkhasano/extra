2694fdb894d10adeb77e4d59d6cd345735d5de05
rkhasano <rkhasano@online.jccc.edu>
2017-03-11 04:59:24 +0500

    Add files via upload

---
+++ Program1WriteYourAge.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    // Instead of '==', there was '='
+    if ( age == 13 )
+    {
+        cout << "Thirteen";
+    }
+
+    // Instead of '==', there was '='
+    if ( age == 18 )
+    {
+        cout << "Eighteen";
+    }
+
+    // Instead of '==', there was '='
+    if ( age == 21 )
+    {
+        cout << "Twenty One";
+    }
+
+    return 0;
+}
